f26befb5b8abbd1880e1efd6673939157ce31844
PratikMahajan <Pratik@mahajan.xyz>
2018-03-24 16:18:54 -0400

    Final Commit

---
+++ Sample Swing Application/src/BusinessLogic/PersonInfo.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+/**
+ *
+ * @author mahajan
+ */
+public class PersonInfo 
+{
+
+private String usrName;
+private String geogData;
+private String dOB;
+private String telNo;
+private String eMail;
+private String ssn;
+//private String medRN;
+//private String healthPBN;
+private String bankAccNo;
+//private String certNo;
+//private String vehID;
+//private String vehSerNo;
+private String vehLicNo;
+private String deviceID;
+//private String deviceSerNo;
+private String linkedIN;
+//private String ipAddr;
+private String uniqueID;
+private String imagePath;
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
+
+
+    public String getUsrName() {
+        return usrName;
+    }
+
+    public void setUsrName(String usrName) {
+        this.usrName = usrName;
+    }
+
+    public String getGeogData() {
+        return geogData;
+    }
+
+    public void setGeogData(String geogData) {
+        this.geogData = geogData;
+    }
+
+    public String getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(String dOB) {
+        this.dOB = dOB;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getBankAccNo() {
+        return bankAccNo;
+    }
+
+    public void setBankAccNo(String bankAccNo) {
+        this.bankAccNo = bankAccNo;
+    }
+
+    public String getVehLicNo() {
+        return vehLicNo;
+    }
+
+    public void setVehLicNo(String vehLicNo) {
+        this.vehLicNo = vehLicNo;
+    }
+
+    public String getDeviceID() {
+        return deviceID;
+    }
+
+    public void setDeviceID(String deviceID) {
+        this.deviceID = deviceID;
+    }
+
+    public String getLinkedIN() {
+        return linkedIN;
+    }
+
+    public void setLinkedIN(String linkedIN) {
+        this.linkedIN = linkedIN;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+
+
+
+
+
+
+
+}
